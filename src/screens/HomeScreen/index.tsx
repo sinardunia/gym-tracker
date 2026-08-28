fd5735040079693d3a9742132634f5987ff5b8ed
sinardunia <fujihalimrabani1601@gmail.com>
2026-08-29 06:35:19 +0700

    feat(home): remove AuthButton from home screen (moved to settings)

--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -8,7 +8,6 @@ import { WeeklySchedule } from '../../components/WeeklySchedule'
 import { ActiveWorkoutBanner } from './ActiveWorkoutBanner'
 import { RoutinePicker } from './RoutinePicker'
 import { SettingsModal } from './SettingsModal'
-import { AuthButton } from '../../components/AuthButton'
 import { getRecommendedWorkout } from '../../lib/selectors'
 import {
   countSets,
@@ -133,8 +132,6 @@ export function HomeScreen({
       {activeWorkout && (
         <ActiveWorkoutBanner workout={activeWorkout} onResume={onResumeWorkout} />
       )}
-
-      <AuthButton />
 
       <ConsistencyWidget stats={consistencyStats} />
 
